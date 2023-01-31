e281b99476a37356895df28f895d7bdb2308e745
JRafael <rafaelmatiashue@gmail.com>
2023-01-31 09:33:37 -0300

    api

---
+++ src/services/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://Root:38714843@cluster0.we0geme.mongodb.net/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
